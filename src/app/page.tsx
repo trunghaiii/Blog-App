b7164bed20597e36a3f19efd81d92992db5c035c
trunghaiii <haitran022001@gmail.com>
2024-04-07 21:38:06 -0400

    fetch blog data from backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
+'use client'
 import AppTable from "@/components/app.table";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  const fetchBlogData = async () => {
+    const response = await fetch("http://localhost:8000/blogs");
+    const blogData = await response.json();
+
+    console.log('blogData: ', blogData);
+
+  }
+
+  useEffect(() => {
+    fetchBlogData()
+  }, [])
+
   return (
     <div>
       <AppTable />
